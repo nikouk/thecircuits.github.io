729ac15aa8ff75eff1b5510b2ae1e61a2c892450
nikouk <nikouk@nikouk.co.uk>
2020-01-06 00:52:18 +0000

    presave

--- js/index.20200106005120.js
+++ js/index.20200106005120.js
@@ -7,11 +7,15 @@ function em1(){var c="uifdjsdvjutcboeAjdmpve/dpn";var addr="mailto:";for(var i=0
 $(document).ready(function() {
 r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=960){$('.js-2').attr('src', (dpi>1) ? 'images/asset-2-276.png' : 'images/asset-2-138.png');
 $('.js-4').attr('src', (dpi>1) ? 'images/single-cover-680.jpg' : 'images/single-cover-340.jpg');
-$('.js-5').attr('src', (dpi>1) ? 'images/pasted-image-78-1.png' : 'images/pasted-image-39-1.png');
-$('.js-6').attr('src', (dpi>1) ? 'images/pasted-image-82-1.png' : 'images/pasted-image-41-1.png');}else{$('.js-2').attr('src', (dpi>1) ? 'images/asset-2-116.png' : 'images/asset-2-58.png');
+$('.js-5').attr('src', (dpi>1) ? 'images/pasted-image-78.png' : 'images/pasted-image-39.png');
+$('.js-6').attr('src', (dpi>1) ? 'images/pasted-image-82.png' : 'images/pasted-image-41.png');
+$('.js-7').attr('src', (dpi>1) ? 'images/spotify_icon_rgb_black-92.png' : 'images/spotify_icon_rgb_black-46.png');
+$('.js-8').attr('src', (dpi>1) ? 'images/pasted-image-100.png' : 'images/pasted-image-50.png');}else{$('.js-2').attr('src', (dpi>1) ? 'images/asset-2-116.png' : 'images/asset-2-58.png');
 $('.js-4').attr('src', (dpi>1) ? 'images/single-cover-232.jpg' : 'images/single-cover-116.jpg');
-$('.js-5').attr('src', (dpi>1) ? 'images/pasted-image-44.png' : 'images/pasted-image-22.png');
-$('.js-6').attr('src', (dpi>1) ? 'images/pasted-image-42.png' : 'images/pasted-image-21.png');}}};
+$('.js-5').attr('src', (dpi>1) ? 'images/pasted-image-26.png' : 'images/pasted-image-13.png');
+$('.js-6').attr('src', (dpi>1) ? 'images/pasted-image-28.png' : 'images/pasted-image-14.png');
+$('.js-7').attr('src', (dpi>1) ? 'images/spotify_icon_rgb_black-30.png' : 'images/spotify_icon_rgb_black-15.png');
+$('.js-8').attr('src', (dpi>1) ? 'images/pasted-image-44-1.png' : 'images/pasted-image-22-1.png');}}};
 if(!window.HTMLPictureElement){$(window).resize(r);r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 var wl = new woolite();
