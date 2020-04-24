02dfbb42a6e669fc74009488494b93849a519b7f
nikouk <nikouk@nikouk.co.uk>
2020-04-24 19:56:22 +0100

    errors

---
+++ js/gallery.20200424195421.js
@@ -0,0 +1,53 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+var lwi=-1;function thresholdPassed(){var w=$(window).width();var p=false;var cw=0;if(w>=960){cw++;}if(lwi!=cw){p=true;}lwi=cw;return p;}
+function em1(){var c="uifdjsdvjutcboeAjdmpve/dpn";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
+
+$(document).ready(function() {
+r=function(){if(thresholdPassed()){dpi=window.devicePixelRatio;if($(window).width()>=960){$('.js-26').attr('src', (dpi>1) ? 'images/asset-2-276.png' : 'images/asset-2-138.png');
+$('.js-28').attr('src', (dpi>1) ? 'images/7th-16-1100-1.jpg' : 'images/7th-16-550-1.jpg');
+$('.js-29').attr('src', (dpi>1) ? 'images/7th-12-464-1.jpg' : 'images/7th-12-232-1.jpg');
+$('.js-30').attr('src', (dpi>1) ? 'images/7th-7-472-1.jpg' : 'images/7th-7-236-1.jpg');
+$('.js-31').attr('src', (dpi>1) ? 'images/29-03-19-9-472-1.jpg' : 'images/29-03-19-9-236-1.jpg');
+$('.js-32').attr('src', (dpi>1) ? 'images/7th-15-470-1.jpg' : 'images/7th-15-235-1.jpg');
+$('.js-33').attr('src', (dpi>1) ? 'images/29-03-19-10-470-1.jpg' : 'images/29-03-19-10-235-1.jpg');
+$('.js-34').attr('src', (dpi>1) ? 'images/r1-01686-0005-1100-1.jpg' : 'images/r1-01686-0005-550-1.jpg');
+$('.js-35').attr('src', (dpi>1) ? 'images/r1-01686-0014-1100-1.jpg' : 'images/r1-01686-0014-550-1.jpg');
+$('.js-36').attr('src', (dpi>1) ? 'images/7th-4-464-1.jpg' : 'images/7th-4-232-1.jpg');
+$('.js-37').attr('src', (dpi>1) ? 'images/7th-5-470-1.jpg' : 'images/7th-5-235-1.jpg');
+$('.js-38').attr('src', (dpi>1) ? 'images/29-03-19-6-472-1.jpg' : 'images/29-03-19-6-236-1.jpg');
+$('.js-39').attr('src', (dpi>1) ? 'images/7th-3-470-1.jpg' : 'images/7th-3-235-1.jpg');
+$('.js-40').attr('src', (dpi>1) ? 'images/r1-01686-0027-470-1.jpg' : 'images/r1-01686-0027-235-1.jpg');
+$('.js-41').attr('src', (dpi>1) ? 'images/r1-01686-0004-1100-1.jpg' : 'images/r1-01686-0004-550-1.jpg');
+$('.js-42').attr('src', (dpi>1) ? 'images/7th-17-1100-1.jpg' : 'images/7th-17-550-1.jpg');
+$('.js-43').attr('src', (dpi>1) ? 'images/1stnov-2-472-1.jpg' : 'images/1stnov-2-236-1.jpg');
+$('.js-44').attr('src', (dpi>1) ? 'images/1stnov-1-470-1.jpg' : 'images/1stnov-1-235-1.jpg');
+$('.js-45').attr('src', (dpi>1) ? 'images/r1-01686-0026-1100-1.jpg' : 'images/r1-01686-0026-550-1.jpg');}else{$('.js-26').attr('src', (dpi>1) ? 'images/asset-2-116.png' : 'images/asset-2-58.png');
+$('.js-28').attr('src', (dpi>1) ? 'images/7th-16-366-1.jpg' : 'images/7th-16-183-1.jpg');
+$('.js-29').attr('src', (dpi>1) ? 'images/7th-12-156-1.jpg' : 'images/7th-12-78-1.jpg');
+$('.js-30').attr('src', (dpi>1) ? 'images/7th-7-158-1.jpg' : 'images/7th-7-79-1.jpg');
+$('.js-31').attr('src', (dpi>1) ? 'images/29-03-19-9-158-1.jpg' : 'images/29-03-19-9-79-1.jpg');
+$('.js-32').attr('src', (dpi>1) ? 'images/7th-15-156-1.jpg' : 'images/7th-15-78-1.jpg');
+$('.js-33').attr('src', (dpi>1) ? 'images/29-03-19-10-158-1.jpg' : 'images/29-03-19-10-79-1.jpg');
+$('.js-34').attr('src', (dpi>1) ? 'images/r1-01686-0005-366-1.jpg' : 'images/r1-01686-0005-183-1.jpg');
+$('.js-35').attr('src', (dpi>1) ? 'images/r1-01686-0014-366-1.jpg' : 'images/r1-01686-0014-183-1.jpg');
+$('.js-36').attr('src', (dpi>1) ? 'images/7th-4-156-1.jpg' : 'images/7th-4-78-1.jpg');
+$('.js-37').attr('src', (dpi>1) ? 'images/7th-5-156-1.jpg' : 'images/7th-5-78-1.jpg');
+$('.js-38').attr('src', (dpi>1) ? 'images/29-03-19-6-158-1.jpg' : 'images/29-03-19-6-79-1.jpg');
+$('.js-39').attr('src', (dpi>1) ? 'images/7th-3-158-1.jpg' : 'images/7th-3-79-1.jpg');
+$('.js-40').attr('src', (dpi>1) ? 'images/r1-01686-0027-156-1.jpg' : 'images/r1-01686-0027-78-1.jpg');
+$('.js-41').attr('src', (dpi>1) ? 'images/r1-01686-0004-366-1.jpg' : 'images/r1-01686-0004-183-1.jpg');
+$('.js-42').attr('src', (dpi>1) ? 'images/7th-17-366-1.jpg' : 'images/7th-17-183-1.jpg');
+$('.js-43').attr('src', (dpi>1) ? 'images/1stnov-2-158-1.jpg' : 'images/1stnov-2-79-1.jpg');
+$('.js-44').attr('src', (dpi>1) ? 'images/1stnov-1-156-1.jpg' : 'images/1stnov-1-78-1.jpg');
+$('.js-45').attr('src', (dpi>1) ? 'images/r1-01686-0026-366-1.jpg' : 'images/r1-01686-0026-183-1.jpg');}}};
+if(!window.HTMLPictureElement){$(window).resize(r);r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js-25')[0], "2.00s", "0.00s", 1, 0);
+wl.addAnimation($('.js-27')[0], "2.00s", "0.00s", 1, 0);
+wl.start();
+
+});
